6e7e3fe6b24cb119c2aca0af45ca661b9f12b9f3
LAHAYE_romain <romain.lahaye@student.vinci.be>
2020-10-28 12:24:31 +0100

    test

--- Assets/script/WeakSpot.cs
+++ Assets/script/WeakSpot.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class WeakSpot : MonoBehaviour
 {
     public GameObject objectToDestroy;
+    //test
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(objectToDestroy);
+
         }
     }
 }
